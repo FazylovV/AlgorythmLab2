7a63f03a455619bf83d98b2b513b9a00ddc5bf57
FazylovV <vladislav.fazylov04@gmail.com>
2023-10-19 12:20:00 +0500

    recursion task

---
+++ Recursion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Recursion.ExecuteReverse(3, 3));
+        }
+    }
+}
---
+++ Recursion/Recursion.cs
@@ -0,0 +1,30 @@
+namespace Recursion
+{
+    public class Recursion
+    {
+        public static int GetNum(int len, int sum, int k, int s) {
+            if (len == k) 
+            {
+                if (sum == s) 
+                {
+                    return 1;
+                }
+                return 0;
+            }
+            
+            int c = (len == 0 ? 1 : 0);
+            int res = 0;
+            
+            for (int i = c; i < 10; i++) {
+                res += GetNum(len + 1, sum + i, k, s);
+            }
+            
+            return res;
+        }
+
+        public static int ExecuteReverse(int k, int s)
+        {
+            return GetNum(0, 0, k, s);
+        }
+    }
+}
